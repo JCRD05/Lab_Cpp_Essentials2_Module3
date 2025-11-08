bc6e4bfc2dcf282801339d50eed7b89850516da3
Jose Carlos Ruiz Diaz <josecarlosruizdiaz05@gmail.com>
2025-11-08 17:06:21 -0600

    Lab 3.1.10

---
+++ Lab_3.1.10/Matrix.cpp
@@ -0,0 +1,109 @@
+#include "Matrix.h"
+#include <iostream>
+#include <random>
+
+void Matrix::print() const
+{
+	for (int row = 1; row <= m_rowDimension; row++) 
+	{
+		for (int column = 1; column <= m_columnDimension; column++) 
+		{
+			std::cout << at(row, column) << "\t";
+		}
+		std::cout << '\n';
+	}
+	std::cout << '\n';
+}
+
+const double& Matrix::at(const int row, const int column) const
+{
+	const bool OUT_OF_RANGE = row < 1 || row > m_rowDimension || column < 1 || column > m_columnDimension;
+	if (OUT_OF_RANGE)
+	{
+		throw std::out_of_range("index out of matrix range");
+	}
+	return m_values[(row - 1) * m_columnDimension + (column - 1)];
+}
+
+double& Matrix::at(const int row, const int column)
+{
+	const bool OUT_OF_RANGE = row < 1 || row > m_rowDimension || column < 1 || column > m_columnDimension;
+	if (OUT_OF_RANGE)
+	{
+		throw std::out_of_range("index out of matrix range");
+	}
+	return m_values[(row - 1) * m_columnDimension + (column - 1)];
+}
+
+void Matrix::setValue(const double value, const int row, const int column)
+{
+	const bool OUT_OF_RANGE = row < 1 || row > m_rowDimension || column < 1 || column > m_columnDimension;
+
+	if (OUT_OF_RANGE)
+	{
+		throw std::out_of_range("index out of matrix range");
+	}
+
+	m_values[(row - 1) * m_columnDimension + (column - 1)] = value;
+}
+
+void Matrix::fill()
+{
+	// Create a random device to obtain a seed
+	std::random_device random;
+
+	// Seed a Mersenne Twister engine
+	std::mt19937 gen(random()); 
+
+	// Create a uniform integer distribution for numbers between 0 and 9
+	std::uniform_int_distribution<> distrib(0, 9);
+
+	for (size_t i = 0; i < m_values.size(); ++i)
+	{
+		m_values[i] = distrib(gen);
+	}
+}
+
+void Matrix::fill(const double value)
+{
+	for (size_t i = 0; i < m_values.size(); ++i)
+	{
+		m_values[i] = value;
+	}
+}
+
+void Matrix::fill(const Matrix& other)
+{
+	const bool DIFFERENT_DIMENSIONS = m_rowDimension != other.getRowDimension() || m_columnDimension != other.getColumnDimension();
+
+	if (DIFFERENT_DIMENSIONS)
+	{
+		throw matrix_exception("different matrices dimensions");
+	}
+
+	m_values = other.m_values;
+}
+
+void Matrix::add(const Matrix& other)
+{
+	const bool DIFFERENT_DIMENSIONS = m_rowDimension != other.getRowDimension() || m_columnDimension != other.getColumnDimension();
+
+	if (DIFFERENT_DIMENSIONS)
+	{
+		throw matrix_exception("different matrices dimensions");
+	}
+
+	for (size_t i = 0; i < m_values.size(); ++i)
+	{
+		m_values[i] += other.m_values[i];
+	}
+}
+
+Matrix Matrix::operator+(const Matrix& other) const
+{
+	Matrix result = *this;
+
+	result.add(other);
+
+	return result;
+}
---
+++ Lab_3.1.10/Matrix.h
@@ -0,0 +1,56 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+
+class matrix_exception : public std::runtime_error
+{
+public:
+	matrix_exception(const char* msg) : runtime_error(msg) {}
+};
+
+class Matrix
+{
+private:
+	int m_rowDimension;
+	int m_columnDimension;
+	std::vector<double> m_values;
+
+public:
+	// Constructors
+	Matrix(const int rowDimension, const int columnDimension) :
+		m_rowDimension(rowDimension), m_columnDimension(columnDimension)
+	{
+		try
+		{
+			if (rowDimension < 1 || columnDimension < 1)
+			{
+				throw std::invalid_argument("invalid input, the matrix dimensions should be higher than zero.");
+			}
+			m_values.resize(rowDimension * columnDimension);
+		}
+		catch (std::invalid_argument& e)
+		{
+			std::cout << e.what();
+		}
+	}
+
+	// Getters
+	inline int getRowDimension() const { return m_rowDimension; }
+	inline int getColumnDimension() const { return m_columnDimension; }
+
+	// Methods
+	void print() const;
+	const double& at(const int row, const int column) const;
+	double& at(const int row, const int column);
+	void setValue(const double value, const int row, const int column);
+	void fill();
+	void fill(const double value);
+	void fill(const Matrix& other);
+	void add(const Matrix& other);
+	Matrix operator+(const Matrix& other) const;
+};
+
+#endif 
