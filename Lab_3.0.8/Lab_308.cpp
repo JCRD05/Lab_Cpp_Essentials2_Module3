0a44f63b92c38f7100d67b88c9dc68ecac368400
Jose Carlos Ruiz Diaz <josecarlosruizdiaz05@gmail.com>
2025-11-07 22:28:45 -0600

    Lab 3.0.8

---
+++ Lab_3.0.8/Lab_308.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+static constexpr int DIVIDE_BY_ZERO = 111;
+
+int main()
+{
+	constexpr int dividend = 8;
+	int divisor{};
+	int result{};
+	std::cin >> divisor;
+	try
+	{
+		if (divisor == 0) { throw "Invalid input: you can't divide by zero"; }
+		result = dividend / divisor;
+	}
+	catch (const char* errorMsg) 
+	{
+		std::cout << errorMsg;
+		return 0;
+	}
+
+	std::cout << result << '\n';
+}
